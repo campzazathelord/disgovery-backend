eab8adb2fbeadee3f566927eb427144ed51937e9
ktns <pom-2551@hotmail.com>
2022-02-02 21:09:48 +0700

    chore: added gitignore

--- src/models/Translation.js
+++ src/models/Translation.js
@@ -1,33 +1,46 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../db/database')
+const Sequelize = require("sequelize");
+const sequelize = require("../db/database");
 
-const Translation = sequelize.define('translations',{
-    table_name:{
-        type: Sequelize.ENUM,
-        values: ['agencies', 'stops', 'routes', 'trips', 'stop_times', 'feed_info', 'pathways', 'levels', 'attributions'],
-        allowNull:false
+const Translation = sequelize.define(
+    "translations",
+    {
+        table_name: {
+            type: Sequelize.ENUM,
+            values: [
+                "agencies",
+                "stops",
+                "routes",
+                "trips",
+                "stop_times",
+                "feed_info",
+                "pathways",
+                "levels",
+                "attributions",
+            ],
+            allowNull: false,
+        },
+        field_name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        language: {
+            type: Sequelize.STRING(10),
+            allowNull: false,
+        },
+        translation: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+        },
+        record_id: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        record_sub_id: Sequelize.STRING,
     },
-    field_name:{
-        type:Sequelize.STRING,
-        allowNull:false
+    {
+        timestamps: false,
+        createdAt: false,
+        updatedAt: false,
     },
-    language :{
-        type:Sequelize.STRING(10),
-        allowNull:false 
-    },
-    translation:{
-        type:Sequelize.TEXT,
-        allowNull:false
-    },
-    record_id:{
-        type:Sequelize.STRING,
-        allowNull:false
-    },
-    record_sub_id :Sequelize.STRING
-
-},{
-    timestamps: false,
-    createdAt: false,
-    updatedAt: false
-  })
-module.exports = Translation
+);
+module.exports = Translation;
