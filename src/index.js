b79a91ea1db56f9091e898d6cbdbbe86215cf2bd
ktns <pom-2551@hotmail.com>
2022-02-02 21:56:40 +0700

    chore: removed redundant codes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 require("dotenv").config();
-const express = require('express')
-const sequelize = require('../src/db/database')
-const btsRouter = require('./routers/bts')
+const express = require("express");
+const sequelize = require("../src/db/database");
+const btsRouter = require("./routers/bts");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(btsRouter)
-sequelize.sync().then(()=>{
-    app.listen(process.env.PORT || 3000, () => {
-        console.log('Server is up on port ' + process.env.PORT || 3000)
-    })
-}).catch(()=>{
-    console.log('error')
-})
+app.use(express.json());
+app.use(btsRouter);
 
-
+app.listen(port, () => {
+    console.log("Server is up on port " + port);
+});
