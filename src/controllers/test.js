cb89a7f3bdfd9a19ee64b9966d0a135b5f92dd63
Rapeekorn Boonribsong <campcampp@Rapeekorns-MacBook-Pro.local>
2022-02-04 00:30:28 +0700

    add functions

--- src/controllers/test.js
+++ src/controllers/test.js
@@ -4,5 +4,5 @@ exports.test = function (req, res) {
     console.log(req.params.id);
     console.log(req.query.name);
 
-    return res.status(APIStatus.OK.status).send(200);
+    return res.status(APIStatus.OK.status).send(APIStatus.OK);
 };
