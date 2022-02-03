8bdf3c3ba680cd808b9a6d292a034523c9e85023
Rapeekorn Boonribsong <campcampp@Rapeekorns-MacBook-Pro.local>
2022-02-04 00:27:10 +0700

    test

--- src/controllers/test.js
+++ src/controllers/test.js
@@ -4,5 +4,5 @@ exports.test = function (req, res) {
     console.log(req.params.id);
     console.log(req.query.name);
 
-    return res.status(APIStatus.OK.status).send(APIStatus.OK);
+    return res.status(APIStatus.OK.status).send(200);
 };
