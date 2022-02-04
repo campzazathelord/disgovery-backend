c21ce2a6f1c7c9fe862e1d95a7dca774f2625ccf
ktns <pom-2551@hotmail.com>
2022-02-04 14:33:45 +0700

    feat: changed .env call to GOOGLE_MAPS_API_KEY

--- src/functions/PlacesDetails.js
+++ src/functions/PlacesDetails.js
@@ -6,7 +6,7 @@ const placesAutoComplete = async (str) => {
         url: `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${String(
             str,
         )}&location=13.7451079,100.5320582&radius=30000&components=country:TH&language=en&key=${
-            process.env.GOOGLE_API_KEY
+            process.env.GOOGLE_MAPS_API_KEY
         }`,
         headers: {},
     });
@@ -16,7 +16,7 @@ const placesAutoComplete = async (str) => {
         url: `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${String(
             str,
         )}&location=13.7451079,100.5320582&radius=30000&components=country:TH&language=th&key=${
-            process.env.GOOGLE_API_KEY
+            process.env.GOOGLE_MAPS_API_KEY
         }`,
         headers: {},
     });
@@ -32,9 +32,10 @@ const placeCoords = async (arr) => {
     for (let x of arr.resultEn) {
         const latitude = await axios({
             method: "get",
-            url: `https://maps.googleapis.com/maps/api/place/details/json?input=bar&placeid=${x.place_id}&key=${process.env.GOOGLE_API_KEY}`,
+            url: `https://maps.googleapis.com/maps/api/place/details/json?input=bar&placeid=${x.place_id}&key=${process.env.GOOGLE_MAPS_API_KEY}`,
             headers: {},
         });
+
         const tmpObj = {
             name: {
                 en: x.structured_formatting.main_text,
@@ -50,11 +51,14 @@ const placeCoords = async (arr) => {
         };
         newArr.push(tmpObj);
     }
+
     for (let [i, value] of arr.resultTh.entries()) {
         newArr[i].address.th = value.structured_formatting.secondary_text;
     }
+
     return newArr;
 };
+
 module.exports = {
     placesAutoComplete,
     placeCoords,
