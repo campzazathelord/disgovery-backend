1574711fba7cb5f3c0f5866e64a4d0cfd1004075
ktns <pom-2551@hotmail.com>
2022-02-17 22:28:44 +0700

    feat: api 2-2 fetches all stops on server start, works in both en and th

--- src/functions/get-all-stops.js
+++ src/functions/get-all-stops.js
@@ -1,5 +1,38 @@
-const Stop = require("../models/Stop");
+const { QueryTypes } = require("sequelize");
+const sequelize = require("../db/database");
 
 exports.getAllStops = async function () {
-    return await Stop.findAll({});
+    const allStations = await sequelize.query(
+        `
+        select * from stops
+            inner join translations on stops.stop_id=translations.record_id and translations.field_name='stop_name' and translations.table_name='stops'
+        `,
+        {
+            type: QueryTypes.SELECT,
+        },
+    );
+
+    let formattedStops = [];
+
+    for (station of allStations) {
+        formattedStops.push({
+            stop_id: station.stop_id,
+            stop_code: station.stop_code,
+            stop_name_en: station.stop_name,
+            stop_name_th: station.translation,
+            stop_desc: station.stop_desc,
+            stop_lat: station.stop_lat,
+            stop_lon: station.stop_lon,
+            zone_id: station.zone_id,
+            stop_url: station.stop_url,
+            location_type: station.location_type,
+            parent_station: station.parent_station,
+            stop_timezone: station.stop_timezone,
+            wheelchair_boarding: station.wheelchair_boarding,
+            level_id: station.level_id,
+            platform_code: station.platform_code,
+        });
+    }
+
+    return formattedStops;
 };
