d166bd26b512e671a0d8d88294323a976fdfd25b
ktns <pom-2551@hotmail.com>
2022-03-29 22:54:28 +0700

    feat: api 2-2 now supports platforms

--- src/controllers/autocomplete/stations-autocomplete.js
+++ src/controllers/autocomplete/stations-autocomplete.js
@@ -13,9 +13,10 @@ exports.getStationAutocomplete = async function getStationAutocomplete(req, res)
 
         if (!query) return res.send(APIStatus.BAD_REQUEST).status(APIStatus.BAD_REQUEST.status);
 
-        const stationData = Object.values(req.app.get("stops"));
+        const allStops = req.app.get("stops");
+        const fuzzyResults = fuzzySearch(Object.values(allStops), query, max_result);
 
-        const fuzzyResults = fuzzySearch(stationData, query, max_result);
+        let allChildrenStops = [];
         let queryString = "";
 
         let responseObject = {};
@@ -34,12 +35,26 @@ exports.getStationAutocomplete = async function getStationAutocomplete(req, res)
                 trips: [],
             };
 
+            allChildrenStops.push(
+                ...getChildrenStops(fuzzyResults[key].item.stop_id, Object.values(allStops)),
+            );
+
             if (index === 0) {
                 queryString += `select stop_id, routes.route_id, routes.route_long_name, routes.route_short_name, routes.route_type, routes.route_color, trips.trip_id, trips.trip_headsign from (select * from stop_times where stop_times.stop_id='${fuzzyResults[key].item.stop_id}') stop_times inner join trips on stop_times.trip_id=trips.trip_id inner join routes on trips.route_id = routes.route_id `;
             } else {
                 queryString += `union select stop_id, routes.route_id, routes.route_long_name, routes.route_short_name, routes.route_type, routes.route_color, trips.trip_id, trips.trip_headsign from (select * from stop_times where stop_times.stop_id='${fuzzyResults[key].item.stop_id}') stop_times inner join trips on stop_times.trip_id=trips.trip_id inner join routes on trips.route_id = routes.route_id `;
             }
         });
+
+        for (let children of allChildrenStops) {
+            if (queryString === "") {
+                queryString += `select stop_id, routes.route_id, routes.route_long_name, routes.route_short_name, routes.route_type, routes.route_color, trips.trip_id, trips.trip_headsign from (select * from stop_times where stop_times.stop_id='${children}') stop_times inner join trips on stop_times.trip_id=trips.trip_id inner join routes on trips.route_id = routes.route_id `;
+            } else {
+                queryString += `union select stop_id, routes.route_id, routes.route_long_name, routes.route_short_name, routes.route_type, routes.route_color, trips.trip_id, trips.trip_headsign from (select * from stop_times where stop_times.stop_id='${children}') stop_times inner join trips on stop_times.trip_id=trips.trip_id inner join routes on trips.route_id = routes.route_id `;
+            }
+        }
+
+        allChildrenStops = [...new Set(allChildrenStops)];
 
         const queriedTrips = await sequelize.query(queryString, { type: QueryTypes.SELECT });
 
@@ -58,6 +73,24 @@ exports.getStationAutocomplete = async function getStationAutocomplete(req, res)
                         headsign: trip[`trip_headsign`],
                     },
                 ];
+            } else {
+                if (allStops[trip.stop_id].parent_station) {
+                    if (responseObject[allStops[trip.stop_id].parent_station]) {
+                        responseObject[allStops[trip.stop_id].parent_station].trips = [
+                            ...responseObject[allStops[trip.stop_id].parent_station].trips,
+                            {
+                                route_id: trip[`route_id`],
+                                route_name: {
+                                    long_name: trip[`route_long_name`],
+                                    short_name: trip[`route_short_name`],
+                                },
+                                type: trip[`route_type`],
+                                color: trip[`route_color`],
+                                headsign: trip[`trip_headsign`],
+                            },
+                        ];
+                    }
+                }
             }
         }
 
@@ -79,6 +112,23 @@ function fuzzySearch(arr, str, max_result) {
 
     const fuse = new Fuse(arr, options);
 
-    const result = fuse.search(str, { limit: max_result || 6 });
-    return result;
+    const results = fuse.search(str);
+
+    const filteredResults = results.filter((result) => {
+        return result.item.parent_station === null;
+    });
+
+    return filteredResults.slice(0, max_result);
+}
+
+function getChildrenStops(parentStation, allStops) {
+    let childrenStops = [];
+
+    for (let stop of allStops) {
+        if (stop.parent_station === parentStation) {
+            childrenStops.push(stop.stop_id);
+        }
+    }
+
+    return childrenStops;
 }
